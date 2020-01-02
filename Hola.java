02e85d728227a3392de532c82acbedf30bd9be01
maolink <maolink@yahoo.com>
2020-01-01 21:49:51 -0500

    nada

--- Hola.java
+++ Hola.java
@@ -6,7 +6,7 @@ public class Hola{
 	public static void main(String[] args){
 		System.out.println("Hola GIT, bienvenido a mi conjunto de herramientas para programar");
 		Objeto aplicacion = new Objeto ("Mauricio", 40);
-		System.out.printf("El objeto creado tiene las siguinetes caracteristicas\nNombre: %s\nEdad: %d",aplicacion.getNombre(), aplicacion.getEdad());
+		System.out.printf("El objeto creado tiene las siguinetes caracteristicas\nNomre: %s\nEdad: %d",aplicacion.getNombre(), aplicacion.getEdad());
 		System.out.printf("El objeto creado tiene las siguinetes caracteristicas\nEdad: %s\nNombre: %d",aplicacion.getEdad(), aplicacion.getNombre());
 		// esto es un comentario que deberia desaparecer
 		// todo el codigi nuevo se pone en verde
